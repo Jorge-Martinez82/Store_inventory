463b08866ee0fcb055d914df4e4bffb1736388ad
cirbcibg <jmartines@gmail.com>
2023-11-05 18:46:07 +0100

    Fetch comenzado

--- listado.php
+++ listado.php
@@ -1,1 +1,38 @@
 <?php
+$conexionProyecto = new mysqli('localhost', 'gestor', 'secreto', 'proyecto');
+$error = $conexionProyecto->connect_errno;
+if ($error != null) {
+    echo "<p>Error $error conectando a la base de datos: $conexionProyecto->connect_error</p>";
+    die();
+}
+
+$resultado = $conexionProyecto->query('SELECT id, nombre FROM productos');
+$stock = $resultado->fetch_object();
+while ($stock != null) {
+
+    echo "<p>Producto $stock->nombre: $stock->id id.</p>";
+
+    $stock = $resultado->fetch_object();
+
+}
+
+$conexionProyecto->close();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tarea03</title>
+</head>
+<body>
+<h2>Gestión de productos</h2>
+<?php
+
+?>
+
+</body>
+</html>
