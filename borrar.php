5091cfa4a7fdcd64f1a12aa626fa36c2fb5a608f
cirbcibg <jmartines@gmail.com>
2023-11-14 19:52:54 +0100

    borrar comentado y consulta preparada

--- borrar.php
+++ borrar.php
@@ -1,15 +1,22 @@
 <?php
+// Establezco la conexión a la base de datos definida en 'conexion.php'
 require 'conexion.php';
 global $conexionProyecto;
+
+// Compruebo si se ha enviado el id del producto a traves del método POST y de hacerlo defino una variable
 if (isset($_POST['id'])) {
     $id = $_POST['id'];
-    $sql = "DELETE FROM productos WHERE id = $id";
-    if ($conexionProyecto->exec($sql)) {
+    // Creo la consulta, la preparo y la ejecuto para evitar inyección SQL
+    $sql = "DELETE FROM productos WHERE id = :id";
+    $stmt = $conexionProyecto->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    if ($stmt->execute()) {
         echo "Producto de código $id borrado correctamente.";
     } else {
-        echo "Error al borrar el producto: " . $conexionProyecto->error;
+        echo "Error al borrar el producto: " . $stmt->errorInfo()[2];// Imprime el error si lo hay
     }
 }
+// Creo el botón formulario para volver al listado
 echo "<form action='listado.php' method='post'>            
       <input type='submit' value='Volver'>
       </form>";
